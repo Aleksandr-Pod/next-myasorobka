8b26ffa4eea31f93d6ff20886c0325ce5bd25734
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2023-11-21 18:18:28 +0200

    images, footer, menu

---
+++ app/uiComponents/footer/kiwiCode.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const KiwiCode = () => {
+  return (
+    <div className="flex gap-x-4 flex-col lg:flex-row h-[55px] items-center justify-center">
+      <p className="text-base text-txt-main-white opacity-[0.6] text-center">Designed and Development by</p>
+      <Link href="#">
+        <Image src="/kiwicode.png" width={100} height={110} alt="Logo KiwiCode Solution" />
+      </Link>
+    </div>
+  );
+};
+
+export default KiwiCode;
--- app/uiComponents/header.tsx
+++ app/uiComponents/header.tsx
@@ -1,20 +1,19 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import ButtonMain from "@/app/uiComponents/kit/button";
 import Menu from "@/app/uiComponents/header/menu";
-import { scrollToTProducts } from "@/app/utils/scroll";
+import { scrollToProducts } from "@/app/utils/scroll";
 
 const Header = () => {
-
   return (
     <div className="pt-2 mx-auto flex justify-between gap-x-3 text-txt-main-white">
-      <Image src='/logo.png' alt="Logo" width={100} height={110} className="w-[100px] h-[110px] lg:ml-[70px]" />
+      <Image src="/logo.png" alt="Logo" width={100} height={110} className="w-[100px] h-[110px] lg:ml-[70px]" />
       <div className="flex flex-col lg:flex-row xl:py-5 items-center lg:mr-[122px]">
-        <nav className='mr-3 lg:mr-11'>
+        <nav className="mr-3 lg:mr-11">
           <Menu section="header" />
         </nav>
-        <ButtonMain style="redCustom" clickFn={scrollToTProducts} >
+        <ButtonMain style="redCustom" clickFn={scrollToProducts}>
           Замовити смаколики
         </ButtonMain>
       </div>
