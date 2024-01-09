c5d13154353848eac903b5266de8fbca026785c3
Aleksandr-Pod <stdfire@gmail.com>
2024-01-09 19:54:43 +0200

    refactor fetch archived orders

--- app/utils/dataFetching.ts
+++ app/utils/dataFetching.ts
@@ -8,6 +8,7 @@ export async function fetchProducts() {
   try {
     const products = await sql<TProduct>`SELECT * FROM products ORDER BY id`
     return products.rows;
+
   } catch (error) {
     console.error({source: "fetchProduct error", message: error})
   }
@@ -15,12 +16,13 @@ export async function fetchProducts() {
 
 export async function fetchOrders(archived: boolean) {
   noStore();
-  // const request = archived ? `SELECT * FROM orders WHERE archived = true` : `SELECT * FROM orders WHERE archived = false`
-  console.log("fetchOrders ...")
+  console.log("fetchOrders ...");
+
   try {
-    const orders = archived ? await sql<TOrders>`SELECT * FROM orders WHERE archived = true` : await sql<TOrders>`SELECT * FROM orders WHERE archived = false`;
+    const orders = await sql<TOrders>`SELECT * FROM orders WHERE archived = ${archived}`;
+    console.log("Замовлення: ", orders.rows);
     return orders.rows;
-    console.log("Замовлення: " + orders.rows);
+
   } catch (error) {
   console.error({source: "fetchOrders error", message: error})
   }
